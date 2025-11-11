011656e73db68e5a764cb9156090570214dcc574
teste <giovannicorrearodrigues124@gmail.com>
2025-11-10 23:37:03 -0300

    My First Api

---
+++ api/src/main/java/tech/buildrun/api/controller/ApiController.java
@@ -0,0 +1,42 @@
+package tech.buildrun.api.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ApiController {
+
+    private List<String> tasks=new ArrayList<>();
+
+    private ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(path = "/tasks")
+    public ResponseEntity<String> List_Tasks() throws JsonProcessingException {
+        return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
+    }
+
+    @PostMapping(path = "/tasks")
+    public ResponseEntity<Void> createTasks(@RequestBody String task){
+        tasks.add(task);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "/tasks")
+    public ResponseEntity<Void> clearTasks(){
+        tasks=new ArrayList<>();
+        return ResponseEntity.ok().build();
+    }
+
+
+}
